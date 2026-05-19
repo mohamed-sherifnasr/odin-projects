f536061041b3427f4104e02ab0584c0b64adca6f
Mohamed Sherif <236368234+mohamed-sherif.nasr@users.noreply.github.com>
2026-05-19 04:13:24 +0300

    Update Build

--- restaurant-page/webpack.config.js
+++ restaurant-page/webpack.config.js
@@ -4,24 +4,37 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import Module from "node:module";
 
 export default {
-  mode: "development",
+  mode: "production",
+
   entry: "./src/index.js",
+
   output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    publicPath: 'auto',
-    clean: true,
+  filename: "main.js",
+  path: path.resolve(import.meta.dirname, "dist"),
+  publicPath: "/",
+  clean: true,
+},
+  devServer: {
+  static: {
+    directory: path.resolve(import.meta.dirname, "dist"),
   },
+  port: 8080,
+  open: true,
+  historyApiFallback: true,
+},
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
-  ],  module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          }
-      ]
-  }
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
