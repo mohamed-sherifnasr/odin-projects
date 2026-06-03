4989ee6e14f334174529d83e66d8acdd39c3e4cd
Mohamed Sherif <236368234+mohamed-sherif.nasr@users.noreply.github.com>
2026-06-03 08:51:27 +0300

    Boilerplate + touches

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,47 +1,113 @@
+//'HomeWork', 'Math Homework', '1/6/2026', 'high', 'Do it before lunch'
+
 class project {
-    constructor(project){
-        this.project = project;
+    constructor(name){
+        this.id = crypto.randomUUID();
+        this.name = name;
+        this.tasks = [];
     }
-    tasks = [];
+    addTask(task){
+        
+    }
+    createTask(title, description, dueDate, priority, notes){
+        const t = new task(title, description, dueDate, priority, notes);
+        this.tasks.push(t);
+    }
+    removeTask(taskId){
+        this.tasks = this.tasks.filter((t, ind) => t.id !== taskId);
+    }
 }
 
-class todos{
-    constructor(project, title, description, dueDate, priority, notes){
-        this.project = project;
+class task {
+    constructor(title, description, dueDate, priority, notes){
+        this.id = crypto.randomUUID();
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
         this.notes = notes;
-        this.checked = false;
+        this.completed = false;
     }
     toggle(){
-        this.checked = !this.checked;
+        this.completed = !this.completed;
+    }
+    editTitle(newTitle){this.title = newTitle}
+    editDescription(newDescription){this.description = newDescription}
+    editDueDate(newDate){this.newDate = newDate}
+    editPriority(newPriority){this.priority = newPriority}
+    editNotes(newNotes){this.notes = newNotes}
+}
+
+class render{
+    constructor(){
+        this.root = document.querySelector('#app');
+        this.form = document.querySelector('#form');
+        this.projectContainer = document.querySelector('#projects-container');
+        this.tasksContainer = document.querySelector('#tasks-container');
+    }
+    showProjects(projects){
+        projects.forEach((prj)=>{
+            const frag = new DocumentFragment();
+            const pj = document.createElement('p');
+            pj.textContent = prj.name;
+            pj.classList.add('project');
+            frag.append(pj);
+            this.projectContainer.append(frag);
+        })
+    }
+    showTasks(project){
+        const taskHeader = document.createElement('div');
+        taskHeader.classList.add('taskHeader');
+        for (const key of Object.keys(project.tasks[0])){
+                const subject = document.createElement('span');
+                subject.classList.add('subject');
+                subject.textContent = key + " ";
+                taskHeader.append(subject);
+        }
+        this.tasksContainer.append(taskHeader);
+        project.tasks.forEach((task)=>{
+            const taskContainer = document.createElement('div');
+            taskContainer.classList.add('task');
+            for (const value of Object.values(task)){
+                const val = document.createElement('span');
+                val.classList.add('val');
+                val.textContent = value + " ";
+                taskContainer.append(val);
+            }
+            this.tasksContainer.append(taskContainer);
+        })
     }
 }
 
-
-//Controller
 class controller{
-    constructor(){};
-    projects = {};
-    createProject(name){
-        const p = new project(name);
-        this.projects[name] = p.tasks;
+    constructor(){
+        this.projects = [new project('default')];
+        this.render = new render();
     }
-    createTask(prj='default', title, desc, dueD, priority, notes){
-        const t = new todos(prj, title, desc, dueD, priority, notes)
-        this.projects[prj].push(t);
+    createProject(name){
+        const prj = new project(name);
+        this.projects.push(prj);
     }
 }
 
-const control = new controller();
-control.createProject("project1");
-control.createTask('project1', "Do HomeWork", "History Homework hasn't been done!", "15/2/2026", "High", "Do it after lunch");
-control.createTask('project1', "Do HomeWork", "History Homework hasn't been done!", "15/2/2026", "High", "Do it after lunch");
-control.createTask('project1', "Do HomeWork", "History Homework hasn't been done!", "15/2/2026", "High", "Do it after lunch");
-control.createProject("project2");
-control.createTask('project2', "Do HomeWork", "History Homework hasn't been done!", "15/2/2026", "High", "Do it after lunch");
-control.createProject("project3");
-control.createTask('project3', "Do HomeWork", "History Homework hasn't been done!", "15/2/2026", "High", "Do it after lunch");
-console.log(control.projects);
+const c = new controller();
+
+c.projects[0].createTask('HomeWork', 'Math Homework', '1/6/2026', 'high', 'Do it before lunch')
+c.projects[0].createTask('HomeWork', 'Math Homework', '1/6/2026', 'high', 'Do it before lunch')
+c.projects[0].createTask('HomeWork', 'Math Homework', '1/6/2026', 'high', 'Do it before lunch')
+c.projects[0].createTask('HomeWork', 'Math Homework', '1/6/2026', 'high', 'Do it before lunch')
+c.projects[0].createTask('HomeWork', 'Math Homework', '1/6/2026', 'high', 'Do it before lunch')
+c.projects[0].createTask('HomeWork', 'Math Homework', '1/6/2026', 'high', 'Do it before lunch')
+c.projects[0].createTask('HomeWork', 'Math Homework', '1/6/2026', 'high', 'Do it before lunch')
+c.projects[0].createTask('HomeWork', 'Math Homework', '1/6/2026', 'high', 'Do it before lunch')
+c.createProject("prrrr");
+c.createProject("prrrr");
+c.createProject("prrrr");
+c.createProject("prrrr");
+c.createProject("prrrr");
+c.createProject("prrrr");
+c.createProject("prrrr");
+c.createProject("prrrr");
+c.createProject("prrrr");
+c.render.showProjects(c.projects);
+c.render.showTasks(c.projects[0]);
