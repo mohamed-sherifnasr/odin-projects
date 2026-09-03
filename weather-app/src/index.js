46be6ab8cd92dbaeb3ed2279d47047fe8263e911
Mohamed Sherif <236368234+mohamed-sherif.nasr@users.noreply.github.com>
2026-09-03 03:04:27 +0300

    Add Second Component + Minor Fixes

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -10,29 +10,31 @@ const icons = {
   'partly-cloudy-night': "partlyCloudyNightSVG",
   'clear-day': "clearDaySVG",
   'clear-night': "clearNightSVG",
-  'snow-showers-day': "snowyShowersDaySVG",
-  'snow-showers-night': "snowyShowersNightSVG",
+  'snow-showers-day': "snowySVG",
+  'snow-showers-night': "snowySVG",
   'thunder-rain': "thunderRainSVG",
   'thunder-showers-day': "thunderShowersDaySVG",
   'thunder-showers-night': "thunderShowersNightSVG",
   'showers-day': "showersDaySVG",
-  'showers-night': "showersNightSVG"
+  'showers-night': "rainySVG"
 };
 
 //Document Nodes
 const mainSearchBtn = document.getElementById("mainSearchBtn");
+const mainContainer = document.getElementById("mainContainer");
+
+//Holding Response for Future Mutation
+let serverResponse;
 
 //Helper Functions
 const winDirTranslate = function(deg){
     let dir;
-    console.log(deg);
-    console.log("condition: ? ", 90 < deg < 180)
     deg == 0? dir = 'n': 90 > deg > 0? dir = 'ne': deg == 90? 'e': 90 < deg < 180? dir = 'se': deg == 180? dir = 's': 180 < deg < 270? dir = 'sw': deg == 270? dir = 'w': 270 < deg < 360? dir = 'nw': dir = 'err';
-    return dir;
+    return dir.toUpperCase();
 }
 
-const fahrToCelsius = function(deg){
-    return (deg - 32 * (5/9)).toFixed();
+const celsiusToFahr = function(deg){
+    return (deg * (9/5) + 32).toFixed();
 }
 const createIcon = function(iconId, cls = null, id = null, width = 50, height = 50){
     //Define SVG Namespace
@@ -54,6 +56,10 @@ const createIcon = function(iconId, cls = null, id = null, width = 50, height = 
     svg.append(use);
     //returns the newly created element
     return svg;    
+}
+const convertHours = function(time){
+    const timeAry = time.split(":");
+    return ((timeAry[0] % 12) + ":" + timeAry[1])
 }
 const extractData = function(data){
     let polishedData = {};
@@ -102,13 +108,13 @@ const extractData = function(data){
         location: data.resolvedAddress,
         time: data.currentConditions.datetime,
         description: data.days[0].description,
-        feelslike: data.currentConditions.feelslike,
-        temp: data.currentConditions.temp,
-        tempmax: data.days[0].tempmax,
-        tempmin: data.days[0].tempmin,
+        feelslike: data.currentConditions.feelslike.toFixed(),
+        temp: data.currentConditions.temp.toFixed(),
+        tempmax: data.days[0].tempmax.toFixed(),
+        tempmin: data.days[0].tempmin.toFixed(),
         icon: data.currentConditions.icon,
-        sunrise: data.currentConditions.sunrise,
-        sunset: data.currentConditions.sunset,
+        sunrise: convertHours(data.currentConditions.sunrise),
+        sunset: convertHours(data.currentConditions.sunset),
         humidity: data.currentConditions.humidity,
         precipprob: data.currentConditions.precipprob || 0,
         precip: data.currentConditions.precip || 0,
@@ -135,7 +141,7 @@ const renderHeader = function(){
     const searchContainer = document.createElement('form');
     const searchBar = document.createElement('input');
     const searchBtn = document.createElement('button');
-    const searchSVG = createIcon("searchSVG", ".containSVG")
+    const searchSVG = createIcon("searchSVG", ".containSVG", undefined, 30, 30);
 
     searchBar.setAttribute("name", "search")
     logo.textContent = "Weather App";
@@ -152,13 +158,13 @@ const renderHeader = function(){
     return header;
 }
 
-//1-of-3 main component (Quick Data)
-const renderQuickData = function(data){
+//1-of-4 main component (Quick Data)
+const renderQuickData = function(data, cel = true){
     //section container
     const container = document.createElement("section");
     //1-of-3 element (Tempreture + IMG + Cels/Fahr)
     const tempContainer = document.createElement("div");
-    const conditionSVG = createIcon(icons[data.icon], undefined, "PrimarySVG", 120, 120);
+    const conditionSVG = createIcon(icons[data.icon], undefined, "primarySVG", 100, 100);
     console.log("Here!");
     const temp = document.createElement("span");
     const tempButtonsContainer = document.createElement("div");
@@ -177,37 +183,63 @@ const renderQuickData = function(data){
 
     //style
     container.setAttribute("id", "quickRenderContainer");
-
+    // 1-of-3
     tempContainer.setAttribute("id", "tempContainer");
     temp.setAttribute("id", "primaryTemp");
     tempButtonsContainer.setAttribute("id", "tempButtonsContainer");
-    celsius.classList.add("toggleUnit");
-    fahrenheit.classList.add("toggleUnit");
+    celsius.setAttribute("id", "celsius");
+    fahrenheit.setAttribute("id", "fahrenheit");
+    if (cel == true){
+        celsius.classList.add("toggleUnit", "toggled")
+        fahrenheit.classList.add("toggleUnit", "unToggledUnit");
+    } else {
+        fahrenheit.classList.add("toggleUnit", "toggled")
+        celsius.classList.add("toggleUnit", "unToggledUnit");
+    }
     divider.classList.add('divide');
-    
+    // 2-of-3
     humContainer.setAttribute("id", "humContainer");
     humidity.classList.add("metric");
     wind.classList.add("metric");
     uvIndex.classList.add("metric");
-
+    // 3-of-3
     locContainer.setAttribute("id", "locContainer");
     location.setAttribute("id", "location");
     condition.setAttribute("id", "condition");
 
     //inject data
     //1-of-3
-    temp.textContent = data.temp;
+    if (cel == true){
+        temp.textContent = data.temp;
+    } else {
+        temp.textContent = celsiusToFahr(data.temp)
+    }
     celsius.textContent = '°C';
     fahrenheit.textContent = '°F';
     divider.textContent = "|";
     //2-of-3
     humidity.textContent = "Humidity: " + data.humidity + "%";
-    wind.textContent = "Wind: " + data.windspeed + " km/h"+ " "+ winDirTranslate(data.winddir);
+    wind.textContent = "Wind: " + data.windspeed + " KM/H"+ " "+ winDirTranslate(data.winddir);
     uvIndex.textContent = "UV Index: " + data.uvindex;
     //3-of-3
+    console.log(data, " ", data.location);
     location.textContent = data.location.split(',')[0];
-    condition.textContent =  data.description;
+    condition.textContent =  "~ " + data.description;
 
+    //Interactivity
+    const toggleUnit = function(e){
+        console.log('clicked!');
+        if(e.target.classList.contains("toggled")) {console.log("element is toggled! Returning ..."); return;}
+        if(e.target.classList.contains("unToggledUnit")){
+            console.log("Element is untoggled! Transversing ...")
+            if(e.target.id == "fahrenheit"){
+                console.log("Fahrenheit that isn't toggled! Rendering! ...");
+                renderMain(serverResponse, false);
+            } else {console.log("Celsius that isn't toggled! Rendering ..."); renderMain(serverResponse)}
+        }
+    }
+    fahrenheit.addEventListener("click", toggleUnit, {once: true});
+    celsius.addEventListener("click", toggleUnit, {once: true});
     //Append
     // 1-of-3
     tempButtonsContainer.append(celsius, divider, fahrenheit)
@@ -220,25 +252,87 @@ const renderQuickData = function(data){
     container.append(tempContainer, humContainer, locContainer);
     return container;
 }   
+// 2-of-4 main component (Secondary Data)
+const renderSecondaryData = function(data, cel = true){
+    //create container Elements
+    const container = document.createElement("div");
+    const feelsLikeCon = document.createElement("div");
+    const highCon = document.createElement("div");
+    const lowCon = document.createElement("div");
+    const pressureCon = document.createElement("div");
+    const sunRiseCon = document.createElement("div");
+    const sunSetCon = document.createElement("div");
+    //create Internal Elements
+    const feelsLike = document.createElement("p");
+    const feelsLikeValue = document.createElement("p");
+    
+    const high = document.createElement("p");
+    const highValue = document.createElement("p");
+    
+    const low = document.createElement("p");
+    const lowValue = document.createElement("p");
+    
+    const pressure = document.createElement("p");
+    const pressureValue = document.createElement("p");
+    
+    const sunRise = document.createElement("p");
+    const sunRiseValue = document.createElement("p");
 
+    const sunSet = document.createElement("p");
+    const sunSetValue = document.createElement("p");
+    //style
+    //Containers
+    container.classList.add("secondaryDataContainer")
+    console.log('running!');
+    [feelsLikeCon, highCon, lowCon, pressureCon, sunRiseCon, sunSetCon].forEach(el=>{
+        el.classList.add('secondaryDataElementContainer');
+    });
+    //Titles
+    [feelsLike, high, low, pressure, sunRise, sunSet].forEach(el=>{
+        el.classList.add('secondaryDataTitle');
+    });
+    //Values
+    [feelsLikeValue, highValue, lowValue, pressureValue, sunRiseValue, sunSetValue].forEach(el=>{
+        el.classList.add('secondaryDataValue');
+    });
 
-//Main
-const renderMain = function(){
+    //inject Data
+    // Titles
+    feelsLike.textContent = "Feels Like";
+    high.textContent = "High";
+    low.textContent = "Low";
+    pressure.textContent = "Pressure";
+    sunRise.textContent = "Sun Rise";
+    sunSet.textContent = "Sun Set";
+    //Data
+    feelsLikeValue.textContent = cel == true? data.feelslike + "°C": celsiusToFahr(data.feelslike) + "°F";
+    highValue.textContent = cel == true? data.tempmax + "°C": celsiusToFahr(data.tempmax) + "°F";
+    lowValue.textContent = cel == true? data.tempmin + "°C": celsiusToFahr(data.tempmin) + "°F";
+    pressureValue.textContent = data.pressure + " hPa";
+    sunRiseValue.textContent = data.sunrise + " AM";
+    sunSetValue.textContent = data.sunset + " PM";
+    //Append
+    feelsLikeCon.append(feelsLike, feelsLikeValue);
+    highCon.append(high, highValue);
+    lowCon.append(low, lowValue);
+    pressureCon.append(pressure, pressureValue);
+    sunRiseCon.append(sunRise, sunRiseValue);
+    sunSetCon.append(sunSet, sunSetValue);
+    container.append(feelsLikeCon, highCon, lowCon, pressureCon, sunRiseCon, sunSetCon);
 
+    //return
+    return container;
 }
 
-
-
 //Main Renderer
-const renderContent = function (data = null){
-    let p = document.createElement("p");
-    p.textContent = JSON.stringify(data);
+const renderMain = function (data = null, cel = true){
     //Instantiate Page Components
     let header = renderHeader();
-    let quick = renderQuickData(data);
+    let quick = renderQuickData(data, cel);
+    let secondary = renderSecondaryData(data, cel)
     let main = document.createElement("main");
     //Re-Draw Screen
-    main.append(header, quick);
+    main.append(header, quick, secondary);
     document.body.replaceChildren(main);
     //Tiny Fixes
     document.body.style.alignItems = "flex-start";
@@ -255,17 +349,12 @@ const handleClick = async function(e){
         let text = await response.text();
         if(!response.ok) throw new Error(text);
         data = JSON.parse(text);
-        let polishedData = extractData(data);
-        console.log()
-        renderContent(polishedData);
+        serverResponse = extractData(data);
+        renderMain(serverResponse);
     } catch(err){ 
         console.error(err);
-        //Further Handle Errors (Show on Screen)
-        return
+
     }
-    console.log(data);
-    let x = data;
-    return x;
 }
 
 //Search Button (Main Trigger for the Fetch)
